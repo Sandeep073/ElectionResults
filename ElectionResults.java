686f02b138f8428bf42f3d556bbd7583dac37d42
Sandeep Diviti <85623700+Sandeep073@users.noreply.github.com>
2024-03-12 18:07:53 -0400

    Update ElectionResults.java

--- ElectionResults.java
+++ ElectionResults.java
@@ -6,90 +6,96 @@ import java.util.*;
 
 class Region {
     String name;
-    Map<String, Integer> analysis = new HashMap<String, Integer>();
+    Map<String, Integer> analysis = new HashMap<>();
     int invalid = 0;
 }
 
-public class ElectionResults {
+public class ElectionResults{
 
-    public static void main(String[] args) throws IOException {
-        List<Integer> voters = new ArrayList<Integer>();
-        Map<String, Integer> total_results = new HashMap<String, Integer>();
-        List<Region> regions = new ArrayList<Region>();
+    public static void main(String[] args) {
+        List<Integer> voters = new ArrayList<>();
+        Map<String, Integer> totalResults = new HashMap<>();
+        List<Region> regions = new ArrayList<>();
         String file = "C:\\Users\\divit\\eclipse-workspace\\VotersProject\\src\\com\\example\\votersproject\\votes.dat";
-        FileReader fr;
 
-        try {
-            fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr);
-            String initial;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
             Region currentRegion = null;
 
-            while ((initial = br.readLine()) != null) {
-                if (initial.equals("//")) {
-                    while (!(initial = br.readLine()).equals("&&")) {
-                        if (initial.equals("//")) {
+            while ((line = br.readLine()) != null) {
+                if (line.equals("//")) {
+                    while (!(line = br.readLine()).equals("&&")) {
+                        if (line.equals("//"))
                             continue;
-                        } else if (Character.isDigit(initial.charAt(0))) {
-                            String[] vt = initial.split(" ");
-                            String[] slicedArray = Arrays.copyOfRange(vt, 1, vt.length);
-
-                            Integer voterId = Integer.valueOf(vt[0]);
-                            if (voters.contains(voterId) || vt.length > 4 || vt.length < 2 || !allKeysPresent(currentRegion, slicedArray)) {
-                                currentRegion.invalid += 1;
-                            } else {
-                                voters.add(voterId);
-                                for (int i = 0; i < slicedArray.length; i++) {
-                                    String votedFor = slicedArray[i];
-                                    Integer currentVotes = currentRegion.analysis.get(votedFor);
-                                    currentVotes += i * (-1) + 3;
-                                    currentRegion.analysis.put(votedFor, currentVotes);
-                                    Integer totalCurrentVotes = total_results.get(votedFor);
-                                    totalCurrentVotes += i * (-1) + 3;
-                                    total_results.put(votedFor, totalCurrentVotes);
-                                }
-                            }
+                        else if (Character.isDigit(line.charAt(0))) {
+                            processVote(line, voters, currentRegion, totalResults);
                         } else {
-                            for (Region regn : regions) {
-                                if (regn.name.equals(initial)) {
-                                    currentRegion = regn;
-                                }
-                            }
+                            currentRegion = findRegion(line, regions);
                         }
                     }
-                } else if (initial.contains("/")) {
-                    String[] tmp = initial.split("/");
-                    char[] cnts = tmp[1].toCharArray();
-                    Region r = new Region();
-                    r.name = tmp[0];
-                    for (char cntst : cnts) {
-                        r.analysis.put(String.valueOf(cntst), 0);
-                        if (!(total_results.containsKey(String.valueOf(cntst)))) {
-                            total_results.put(String.valueOf(cntst), 0);
-                        }
-                    }
-                    regions.add(r);
+                } else if (line.contains("/")) {
+                    createRegion(line, regions, totalResults);
                 }
 
-                if (initial.equals("&&")) {
+                if (line.equals("&&")) {
                     break;
                 }
             }
-            ElectionResults(regions, total_results);
+            printElectionResults(regions, totalResults);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void ElectionResults(List<Region> regions, Map<String, Integer> total_results) {
+    private static void processVote(String voteLine, List<Integer> voters, Region currentRegion, Map<String, Integer> totalResults) {
+        String[] vt = voteLine.split(" ");
+        String[] slicedArray = Arrays.copyOfRange(vt, 1, vt.length);
+
+        Integer voterId = Integer.valueOf(vt[0]);
+        if (voters.contains(voterId) || vt.length > 4 || vt.length < 2 || !allKeysPresent(currentRegion, slicedArray)) {
+            currentRegion.invalid++;
+        } else {
+            voters.add(voterId);
+            for (int i = 0; i < slicedArray.length; i++) {
+                String votedFor = slicedArray[i];
+                int currentVotes = currentRegion.analysis.getOrDefault(votedFor, 0);
+                currentVotes += i * (-1) + 3;
+                currentRegion.analysis.put(votedFor, currentVotes);
+                totalResults.put(votedFor, totalResults.getOrDefault(votedFor, 0) + i * (-1) + 3);
+            }
+        }
+    }
+
+    private static Region findRegion(String regionName, List<Region> regions) {
+        for (Region region : regions) {
+            if (region.name.equals(regionName)) {
+                return region;
+            }
+        }
+        return null;
+    }
+
+    private static void createRegion(String regionLine, List<Region> regions, Map<String, Integer> totalResults) {
+        String[] tmp = regionLine.split("/");
+        char[] cnts = tmp[1].toCharArray();
+        Region r = new Region();
+        r.name = tmp[0];
+        for (char cntst : cnts) {
+            r.analysis.put(String.valueOf(cntst), 0);
+            totalResults.putIfAbsent(String.valueOf(cntst), 0);
+        }
+        regions.add(r);
+    }
+
+    private static void printElectionResults(List<Region> regions, Map<String, Integer> totalResults) {
         for (Region r : regions) {
             String wonAtRegion = "";
-            Integer currentHigh = 0;
-            if (r.analysis.keySet().size() >= 1) {
-                for (String candidate : r.analysis.keySet()) {
-                    if (r.analysis.get(candidate) > currentHigh) {
-                        currentHigh = r.analysis.get(candidate);
-                        wonAtRegion = candidate;
+            int currentHigh = 0;
+            if (!r.analysis.isEmpty()) {
+                for (Map.Entry<String, Integer> entry : r.analysis.entrySet()) {
+                    if (entry.getValue() > currentHigh) {
+                        currentHigh = entry.getValue();
+                        wonAtRegion = entry.getKey();
                     }
                 }
                 System.out.println(wonAtRegion + "  with score: " + currentHigh + " And No of invalid votes in this region is: " + r.invalid);
@@ -97,25 +103,24 @@ public class ElectionResults {
             }
         }
 
-        String ChiefOfficer = "";
-        Integer currentHigh = 0;
-        for (String cd : total_results.keySet()) {
-            if (total_results.get(cd) > currentHigh) {
-                ChiefOfficer = cd;
-                currentHigh = total_results.get(cd);
+        String chiefOfficer = "";
+        int currentHigh = 0;
+        for (Map.Entry<String, Integer> entry : totalResults.entrySet()) {
+            if (entry.getValue() > currentHigh) {
+                chiefOfficer = entry.getKey();
+                currentHigh = entry.getValue();
             }
         }
-        System.out.println("Chief Officer is: " + ChiefOfficer + "  with total votes: " + currentHigh);
-        System.out.println("Cogratulations : "+ChiefOfficer);
+        System.out.println("Chief Officer is: " + chiefOfficer + "  with total votes: " + currentHigh);
+        System.out.println("Congratulations: " + chiefOfficer);
     }
 
-    private static boolean allKeysPresent(Region currentRegion, String[] slicedarray) {
-        boolean allKeysPresent = true;
-        for (String i : slicedarray) {
+    private static boolean allKeysPresent(Region currentRegion, String[] slicedArray) {
+        for (String i : slicedArray) {
             if (!currentRegion.analysis.containsKey(i)) {
-                allKeysPresent = false;
+                return false;
             }
         }
-        return allKeysPresent;
+        return true;
     }
 }
